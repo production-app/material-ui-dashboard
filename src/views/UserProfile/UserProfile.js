47814d52058c74a68e19d6330bfc8ed582cb9855
ABIODUN ADEBAYO <developerbiodun@gmail.com>
2020-09-28 17:40:32 +0100

    GitHub actions

--- src/views/UserProfile/UserProfile.js
+++ src/views/UserProfile/UserProfile.js
@@ -205,6 +205,8 @@ function UserProfile({
               avatarUrl: imageUrl,
             });
 
+            imageGetter("");
+
             getData({
               email,
               phone: phoneNumber,
